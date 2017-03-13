91064e4d8c16bbb582dc85e7ab983a24508ecb93
Alex MacLean <alex.maclean2000@gmail.com>
2017-03-12 22:30:54 -0700

    worked on data viewing

--- ScoutingFRC/DataViewingActivity.cs
+++ ScoutingFRC/DataViewingActivity.cs
@@ -40,6 +40,7 @@ namespace ScoutingFRC
         private void displayData(List<MatchData> datas)
         {
             FindViewById<TextView>(Resource.Id.textViewTeamNumber).Text = datas[0].teamNumber.ToString();
+            int count = datas.Count;
             string matches = "Matches: ";
             int[] gears = new int[4];
             int[] HighGoals = new int[4];
@@ -47,8 +48,20 @@ namespace ScoutingFRC
             foreach (var matchData in datas)
             {
                 matches += matchData.match + ", ";
-
+                addScoringMethod(matchData.automomous.gears, 0, gears);
+                addScoringMethod(matchData.teleoperated.gears, 2, gears);
+                addScoringMethod(matchData.automomous.highBoiler, 0, HighGoals);
+                addScoringMethod(matchData.teleoperated.highBoiler, 2, HighGoals);
+                addScoringMethod(matchData.automomous.lowBoiler, 0, LowGoals);
+                addScoringMethod(matchData.teleoperated.lowBoiler, 2, LowGoals);
             }
+            double[] high = divide(HighGoals, count);
+            double[] low = divide(LowGoals, count);
+            double[] gear = divide(gears, count);
+
+            FindViewById<TextView>(Resource.Id.textView1).Text = matches;
+
+
         }
 
         private double[] divide(int[] ar, int a)
@@ -63,7 +76,8 @@ namespace ScoutingFRC
 
         private void addScoringMethod(MatchData.PreformaceData.ScoringMethod method, int start, int[] arr)
         {
-            
+            arr[start] += method.successes;
+            arr[start+1] += method.failedAttempts;
         }
     }
 }
