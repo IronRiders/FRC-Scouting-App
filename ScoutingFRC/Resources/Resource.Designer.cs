e4589cb635cd7b10c894971422b3278b505632ad
Alex MacLean <alex.maclean2000@gmail.com>
2017-03-13 21:33:22 -0700

    splash

--- ScoutingFRC/Resources/Resource.Designer.cs
+++ ScoutingFRC/Resources/Resource.Designer.cs
@@ -103,11 +103,11 @@ namespace ScoutingFRC
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f070016
-			public const int button1 = 2131165206;
+			// aapt resource value: 0x7f070017
+			public const int button1 = 2131165207;
 			
-			// aapt resource value: 0x7f070012
-			public const int buttonCollect = 2131165202;
+			// aapt resource value: 0x7f070014
+			public const int buttonCollect = 2131165204;
 			
 			// aapt resource value: 0x7f07000e
 			public const int buttonGearGoal = 2131165198;
@@ -130,8 +130,8 @@ namespace ScoutingFRC
 			// aapt resource value: 0x7f070011
 			public const int buttonSubmit = 2131165201;
 			
-			// aapt resource value: 0x7f070014
-			public const int buttonView = 2131165204;
+			// aapt resource value: 0x7f070016
+			public const int buttonView = 2131165206;
 			
 			// aapt resource value: 0x7f070007
 			public const int checkBox1 = 2131165191;
@@ -139,14 +139,17 @@ namespace ScoutingFRC
 			// aapt resource value: 0x7f070010
 			public const int checkBoxClimb = 2131165200;
 			
-			// aapt resource value: 0x7f070013
-			public const int editText2 = 2131165203;
+			// aapt resource value: 0x7f070015
+			public const int editText2 = 2131165205;
 			
 			// aapt resource value: 0x7f070005
 			public const int editTextMathcNumber = 2131165189;
 			
 			// aapt resource value: 0x7f070002
 			public const int editTextTeamNumber = 2131165186;
+			
+			// aapt resource value: 0x7f070012
+			public const int imageView1 = 2131165202;
 			
 			// aapt resource value: 0x7f070000
 			public const int linearLayout1 = 2131165184;
@@ -157,8 +160,8 @@ namespace ScoutingFRC
 			// aapt resource value: 0x7f070009
 			public const int linearLayout3 = 2131165193;
 			
-			// aapt resource value: 0x7f070017
-			public const int listView1 = 2131165207;
+			// aapt resource value: 0x7f070018
+			public const int listView1 = 2131165208;
 			
 			// aapt resource value: 0x7f070006
 			public const int switchAuto = 2131165190;
@@ -172,23 +175,23 @@ namespace ScoutingFRC
 			// aapt resource value: 0x7f070008
 			public const int textView3 = 2131165192;
 			
-			// aapt resource value: 0x7f070019
-			public const int textView4 = 2131165209;
-			
 			// aapt resource value: 0x7f07001a
-			public const int textView5 = 2131165210;
+			public const int textView4 = 2131165210;
 			
 			// aapt resource value: 0x7f07001b
-			public const int textView6 = 2131165211;
+			public const int textView5 = 2131165211;
 			
 			// aapt resource value: 0x7f07001c
-			public const int textView7 = 2131165212;
+			public const int textView6 = 2131165212;
 			
-			// aapt resource value: 0x7f070018
-			public const int textViewTeamNumber = 2131165208;
+			// aapt resource value: 0x7f07001d
+			public const int textView7 = 2131165213;
 			
-			// aapt resource value: 0x7f070015
-			public const int view1 = 2131165205;
+			// aapt resource value: 0x7f070019
+			public const int textViewTeamNumber = 2131165209;
+			
+			// aapt resource value: 0x7f070013
+			public const int view1 = 2131165203;
 			
 			static Id()
 			{
--- ScoutingFRC/SplashActivity.cs
+++ ScoutingFRC/SplashActivity.cs
@@ -1,11 +1,12 @@
 using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.OS;
 using Android.Util;
 using ScoutingFRC;
 
-[Activity(Theme = "@style/CustomTheme.Splash", MainLauncher = true, NoHistory = true)]
+[Activity(Theme = "@style/CustomTheme.Splash", MainLauncher = true, NoHistory = true, Icon = "@drawable/icon", ScreenOrientation = ScreenOrientation.Portrait)]
 public class SplashActivity : Activity
 {
 
