2572db1ace133429a1d9da42f55d4a4448084ce4
Alex MacLean <alex.maclean2000@gmail.com>
2017-03-15 13:24:12 -0700

    syncing activity

---
+++ ScoutingFRC/SyncDataActivity.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ScoutingFRC
+{
+    [Activity(Label = "SyncDataActivity")]
+    public class SyncDataActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
