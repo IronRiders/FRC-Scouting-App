078bda3621f2d63ab54ab892358165ff371159f8
Alex MacLean <alex.maclean2000@gmail.com>
2017-03-15 20:32:27 -0700

    working on syncing

--- ScoutingFRC/Resources/Resource.Designer.cs
+++ ScoutingFRC/Resources/Resource.Designer.cs
@@ -103,14 +103,23 @@ namespace ScoutingFRC
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f070015
-			public const int autoCompleteTextView1 = 2131165205;
+			// aapt resource value: 0x7f070014
+			public const int autoCompleteTextView1 = 2131165204;
 			
 			// aapt resource value: 0x7f070017
 			public const int button1 = 2131165207;
 			
-			// aapt resource value: 0x7f070014
-			public const int buttonCollect = 2131165204;
+			// aapt resource value: 0x7f070019
+			public const int button2 = 2131165209;
+			
+			// aapt resource value: 0x7f07001b
+			public const int button3 = 2131165211;
+			
+			// aapt resource value: 0x7f07001c
+			public const int button4 = 2131165212;
+			
+			// aapt resource value: 0x7f070013
+			public const int buttonCollect = 2131165203;
 			
 			// aapt resource value: 0x7f07000e
 			public const int buttonGearGoal = 2131165198;
@@ -134,7 +143,10 @@ namespace ScoutingFRC
 			public const int buttonSubmit = 2131165201;
 			
 			// aapt resource value: 0x7f070016
-			public const int buttonView = 2131165206;
+			public const int buttonSync = 2131165206;
+			
+			// aapt resource value: 0x7f070015
+			public const int buttonView = 2131165205;
 			
 			// aapt resource value: 0x7f070007
 			public const int checkBox1 = 2131165191;
@@ -148,9 +160,6 @@ namespace ScoutingFRC
 			// aapt resource value: 0x7f070002
 			public const int editTextTeamNumber = 2131165186;
 			
-			// aapt resource value: 0x7f070012
-			public const int imageView1 = 2131165202;
-			
 			// aapt resource value: 0x7f070000
 			public const int linearLayout1 = 2131165184;
 			
@@ -159,6 +168,9 @@ namespace ScoutingFRC
 			
 			// aapt resource value: 0x7f070009
 			public const int linearLayout3 = 2131165193;
+			
+			// aapt resource value: 0x7f07001a
+			public const int linearLayout4 = 2131165210;
 			
 			// aapt resource value: 0x7f070018
 			public const int listView1 = 2131165208;
@@ -175,23 +187,23 @@ namespace ScoutingFRC
 			// aapt resource value: 0x7f070008
 			public const int textView3 = 2131165192;
 			
-			// aapt resource value: 0x7f07001a
-			public const int textView4 = 2131165210;
+			// aapt resource value: 0x7f07001e
+			public const int textView4 = 2131165214;
 			
-			// aapt resource value: 0x7f07001b
-			public const int textView5 = 2131165211;
+			// aapt resource value: 0x7f07001f
+			public const int textView5 = 2131165215;
 			
-			// aapt resource value: 0x7f07001c
-			public const int textView6 = 2131165212;
+			// aapt resource value: 0x7f070020
+			public const int textView6 = 2131165216;
+			
+			// aapt resource value: 0x7f070021
+			public const int textView7 = 2131165217;
 			
 			// aapt resource value: 0x7f07001d
-			public const int textView7 = 2131165213;
+			public const int textViewTeamNumber = 2131165213;
 			
-			// aapt resource value: 0x7f070019
-			public const int textViewTeamNumber = 2131165209;
-			
-			// aapt resource value: 0x7f070013
-			public const int view1 = 2131165203;
+			// aapt resource value: 0x7f070012
+			public const int view1 = 2131165202;
 			
 			static Id()
 			{
