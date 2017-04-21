6c3ad28c7dddd34b2d3e7b520b07830a12b30ffd
Alex MacLean <alex.maclean2000@gmail.com>
2017-04-21 13:58:09 -0700

    added comments

--- ScoutingFRC/MainActivity.cs
+++ ScoutingFRC/MainActivity.cs
@@ -19,7 +19,6 @@ namespace ScoutingFRC
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
             FindViewById<Button>(Resource.Id.buttonSync).Click += ButtonSync_Click;
             FindViewById<Button>(Resource.Id.buttonPitScouting).Click += ButtonPitScouting_Click;
@@ -28,9 +27,11 @@ namespace ScoutingFRC
             FindViewById<ListView>(Resource.Id.listView1).ItemClick += OnItemClick;
 
             teamDataList = Storage.ReadFromFile("ScoutingData2017") ?? new List<TeamData>();
-
         }
-
+        
+        /// <summary>  
+        /// When an Item is clicked in the list of team data, that team's data is displayed 
+        /// </summary> 
         private void OnItemClick(object sender, AdapterView.ItemClickEventArgs itemClickEventArgs)
         {
             var item = FindViewById<ListView>(Resource.Id.listView1).Adapter.GetItem(itemClickEventArgs.Position);
@@ -62,6 +63,9 @@ namespace ScoutingFRC
             list.Adapter = teamListAdapter;
         }
 
+        /// <summary>  
+        ///  When the button to collect match data is clicked a new MatchData Activity is started and suplyed with the users name and last match
+        /// </summary> 
         private void ButtonCollect_Click(object sender, EventArgs e)
         {
             var myIntent = new Intent(this, typeof(DataCollectionActivity));
@@ -70,22 +74,31 @@ namespace ScoutingFRC
             myIntent.PutExtra("match", upcomingMatch);
             StartActivityForResult(myIntent, 0);
         }
-
+        
+        /// <summary>  
+        ///  When the button to View data is clicked a new DataViewingActivity is started.
+        /// </summary>  
         private void ButtonView_Click(object sender, EventArgs e)
         {
             int number;
-           
             if(int.TryParse(FindViewById<AutoCompleteTextView>(Resource.Id.autoCompleteTextView1).Text, out number)) {
                 DisplayTeamData(number);
             }  
         }
+        
+        /// <summary>  
+        ///  When the button to collect pit data is clicked a new PitData Activity is started and suplyed with the users name
+        /// </summary> 
         private void ButtonPitScouting_Click(object sender, EventArgs eventArgs)
         {
             var myIntent = new Intent(this, typeof(PitScoutingActivity));
             myIntent.PutExtra("name", lastName);
             StartActivityForResult(myIntent, 2);
         }
-
+        
+        /// <summary>  
+        ///  Finds the data for a given Team number and then starts a viewActivity with this data
+        /// </summary> 
         private void DisplayTeamData(int number)
         {
             List<TeamData> teamData = new List<TeamData>();
@@ -99,7 +112,10 @@ namespace ScoutingFRC
                 StartActivity(viewActivity);
             }
         }
-
+        
+        /// <summary>  
+        ///  When the button to sync data is clicked a new sync Activity is started and suplyed with the current data
+        /// </summary> 
         private void ButtonSync_Click(object sender, EventArgs e)
         {
             var myIntent = new Intent(this, typeof(SyncDataActivity));
@@ -107,7 +123,10 @@ namespace ScoutingFRC
             myIntent.PutExtra("currentData", bytes);
             StartActivityForResult(myIntent, 1);
         }
-
+        
+        /// <summary>  
+        ///  When data is recived from another activity it is added to the list and stored
+        /// </summary> 
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
         {
             base.OnActivityResult(requestCode, resultCode, data);
