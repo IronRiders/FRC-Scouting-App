723941969c8a0345342cb8a0fbb5576068ea3eb8
Alex MacLean <alex.maclean2000@gmail.com>
2017-04-21 13:24:07 -0700

    Removed Unnecessary Code

--- ScoutingFRC/TeamData.cs
+++ ScoutingFRC/TeamData.cs
@@ -18,31 +18,5 @@ namespace ScoutingFRC
             TeamData t = obj as TeamData;
             return teamNumber == t.teamNumber && scoutName == t.scoutName && notes == t.notes;
         }
-
-        /*public T Merge<T>(TeamData other) where T : TeamData, new()
-        {
-            T result = new T();
-
-            result.teamNumber = teamNumber;
-            result.scoutName = scoutName;
-            result.notes = notes;
-
-            return result;
-        }
-
-        public static int Merge(int a, int b)
-        {
-            bool a0 = a == 0;
-            bool b0 = b == 0;
-
-            if(!a0 && !b0) {
-                return (a + b) / 2;
-            }
-            else if (a0 || b0) {
-                return Math.Max(a, b);
-            }
-
-            return 0;
-        }*/
     }
 }
